c939a9706a92fa789f8a57b759e5b3ee51f189ec
Manoel Almeida <manoel.almeida@bandtec.com.br>
2020-02-06 00:05:11 -0300

    ListEditForm state and button functions

--- web/src/components/list-edit-form/index.js
+++ web/src/components/list-edit-form/index.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
 function ListEditForm({ onClose }) {
 
+    const [title, setTitle] = useState('');
+
     function cancel() {
         onClose();
+    }
+
+    async function taskEdit() {
+        console.log(title);
+    }
+
+    async function taskDelete() {
+        return;
     }
 
     return (
         <form className="form">
             <h2>List</h2>
             <label>Name</label><br></br>
-            <input type="text"/>
+            <input type="text" onChange={e => setTitle(e.target.value)} value={title}/>
             <div className="button-group">
                 <div className="group-1">
-                    <button className="primary-button">save</button>
-                    <button className="danger-button">delete</button>
+                    <button type="button" className="primary-button" onClick={taskEdit}>save</button>
+                    <button type="button" className="danger-button">delete</button>
                 </div>
-                <button className="secondary-button" onClick={cancel}>cancel</button>
+                <button type="button" className="secondary-button" onClick={cancel}>cancel</button>
             </div>
         </form>
     );
